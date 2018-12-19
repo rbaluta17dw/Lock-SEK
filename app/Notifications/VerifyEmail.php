40fcec2c24c2e5b3fe7a7882b111617ae754f6b0
rbaluta17dw <rbaluta17dw@zubirimanteo.com>
2018-12-19 12:58:14 +0100

    usar el middleware correcto en notificaciones

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -12,83 +12,67 @@ use Illuminate\Support\Facades\Lang;
 
 class VerifyEmail extends Notification
 {
-    use Queueable;
+  /**
+  * The callback that should be used to build the mail message.
+  *
+  * @var \Closure|null
+  */
+  public static $toMailCallback;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
+  /**
+  * Get the notification's channels.
+  *
+  * @param  mixed  $notifiable
+  * @return array|string
+  */
+  public function via($notifiable)
+  {
+    return ['mail'];
+  }
+
+  /**
+  * Build the mail representation of the notification.
+  *
+  * @param  mixed  $notifiable
+  * @return \Illuminate\Notifications\Messages\MailMessage
+  */
+  public function toMail($notifiable)
+  {
+    if (static::$toMailCallback) {
+      return call_user_func(static::$toMailCallback, $notifiable);
+    }
+
+    return (new MailMessage)
+    ->subject(Lang::getFromJson('Verificaion de email'))
+    ->line(Lang::getFromJson('Este mensaje es un mensaje automatico para asegurar que usted ha solicitado el registro de una cuenta en nuestra plataforma.'))
+    ->action(
+      Lang::getFromJson('Verificar email'),
+      $this->verificationUrl($notifiable)
+      )
+      ->line(Lang::getFromJson('Si usted no solicito el registro ignore este mensaje.'));
     }
 
     /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
-    /**
-     * Build the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        if (static::$toMailCallback) {
-            return call_user_func(static::$toMailCallback, $notifiable);
-        }
-
-        return (new MailMessage)
-            ->subject(Lang::getFromJson('Verificaion de email'))
-            ->line(Lang::getFromJson('Este mensaje es un mensaje automatico para asegurar que usted ha solicitado el registro de una cuenta en nuestra plataforma.'))
-            ->action(
-                Lang::getFromJson('Verificar email'),
-                $this->verificationUrl($notifiable)
-            )
-            ->line(Lang::getFromJson('Si usted no solicito el registro ignore este mensaje.'));
-    }
-    /**
-     * Get the verification URL for the given notifiable.
-     *
-     * @param  mixed  $notifiable
-     * @return string
-     */
+    * Get the verification URL for the given notifiable.
+    *
+    * @param  mixed  $notifiable
+    * @return string
+    */
     protected function verificationUrl($notifiable)
     {
-        return URL::temporarySignedRoute(
-            'verification.verify', Carbon::now()->addMinutes(60), ['id' => $notifiable->getKey()]
-        );
+      return URL::temporarySignedRoute(
+        'verification.verify', Carbon::now()->addMinutes(60), ['id' => $notifiable->getKey()]
+      );
     }
 
     /**
-     * Set a callback that should be used when building the notification mail message.
-     *
-     * @param  \Closure  $callback
-     * @return void
-     */
+    * Set a callback that should be used when building the notification mail message.
+    *
+    * @param  \Closure  $callback
+    * @return void
+    */
     public static function toMailUsing($callback)
     {
-        static::$toMailCallback = $callback;
+      static::$toMailCallback = $callback;
     }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
-    }
-}
+  }
