346bff8afce03a8509d84852bffe90609e3ab025
Ibai <ibaialberdi@gmail.com>
2018-12-20 13:24:31 +0100

    merge

--- app/Lock.php
+++ app/Lock.php
@@ -18,11 +18,7 @@ class Lock extends Model
 
     public function privileges()
     {
-<<<<<<< HEAD
-        return $this->belongsToMany('App\User');
-=======
         return $this->belongsToMany('App\User', 'privileges');
 
->>>>>>> b7e713670ad0e613527d18cf495aae23aa0382c4
     }
 }
--- resources/views/pages/key/createKey.blade.php
+++ resources/views/pages/key/createKey.blade.php
@@ -8,41 +8,33 @@
 
 
 <div class="row">
-    
+
     <div class="col-sm-9">
         <h4>Crear Llave</h4>
         <div class="tab-content">
             <hr>
-            
-            
-            
+
+
+
             <form class="form" action="#" method="post">
                 <label for="date">Nombre de la llave</label>
                 <p> <input type="text" name="newKeyName" placeholder="Nombre de la llave" /></p>
-                
-                
-                
-<<<<<<< HEAD
-         
-            
-         
-         
-                
-=======
->>>>>>> b7e713670ad0e613527d18cf495aae23aa0382c4
+
+
+
                <label for="date">Añade un dispositivo</label>
-                
+
                <p><input type="file"  name="newKey" /></p>
 
-                
+
                 <button type="submit" class="btn btn-default btn-primary">Añadir</button>
-                
+
             </form>
             <br>
-    
-            
-            
-            
+
+
+
+
         </div>
     </div><!--/col-9-->
 </div><!--/row-->
