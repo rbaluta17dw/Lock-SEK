7666d94b4394da5ee29136f5d0f335c3a89f1eee
Ibai <ibaialberdi@gmail.com>
2018-12-20 13:14:32 +0100

    Vista register Lock mejor diseño

---
+++ resources/views/pages/lock/registerLock.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.dashboard')
+@section('title', 'LockSEK')
+@section('content')
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /Modal -->
+
+
+
+<div class="card text-center">
+  <div class="card-header">
+    <h1>Registra tu cerradura</h1>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">Para completar el registro de tu cerradura asegurate de tener cerca la cerradura, la necesitaras durante el proceso.</p>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Empezar!
+</button>
+
+
+
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+
+
+
+
+@stop
