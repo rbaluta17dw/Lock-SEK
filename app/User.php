5533198e6e0fb2d9920c71704c9d21141836cc87
Ander <alaboreoca17dw@ikzubirimanteo.com>
2018-12-20 08:43:19 +0100

    modelos

--- app/User.php
+++ app/User.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
+
+    public function locks()
+    {
+        return $this->hasMany('App\Lock');
+    }
     use Notifiable;
 
     /**
