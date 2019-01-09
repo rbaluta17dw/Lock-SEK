358033da26db53c7871f61955d19973ec5a674f3
rbaluta17dw <rbaluta17dw@zubirimanteo.com>
2019-01-09 13:17:18 +0100

    Notificaciones funciona

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -8,16 +8,14 @@ use Illuminate\Http\Request;
 class AjaxController extends Controller
 {
     public function checkNotification(){
+        
         if (true) {
           $this->getNotifications();
-        }else {
-
         }
     }
 
     public function getNotifications(){
-        $notifications = Notification::where('user_id', Auth::user()->id);
-        $titulo = $notifications->title;
-        return $titulo;
+        $notifications = Notification::where('user_id', Auth::user()->id)->get();
+        return $notifications;
     }
 }
--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -1,7 +1,9 @@
 
 $(window).click(function(){
-  $.ajax({url: "https://www.google.es/", success: function(result){
-
-      alert(result);
+  $.ajax({url: "notifications", success: function(result){
+    $('#notifications').html('');
+    for (var i = 0; i < result.length; i++) {
+      $('#notifications').append('<li><a href="#"><div><i class="fa fa-comment fa-fw"></i>'+result[i].title+'<span class="pull-right text-muted small">'+result[i].message+'</span></div></a></li>');
+    }
   }});
 });
