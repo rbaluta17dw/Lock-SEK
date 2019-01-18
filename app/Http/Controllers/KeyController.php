7aa79e50f7f04628bebcc098b33749fd77b631c5
Ander <alaboreoca17dw@ikzubirimanteo.com>
2019-01-18 10:32:09 +0100

    arreglado keycontroller

--- app/Http/Controllers/KeyController.php
+++ app/Http/Controllers/KeyController.php
@@ -15,140 +15,38 @@ use Auth;
 
 class KeyController extends Controller
 {
-
-
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
-
-  /**
-  * Display a listing of the resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
-  public function index()
-  {
-
-    $keys = Key::where('user_id', Auth::user()->id)->get();
-
-    return view('pages/key/keys',['keys'=>$keys]);
-
-
-  }
-
-  /**
-  * Show the form for creating a new resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
-  public function create()
-  {
-    return view('pages/key/create');
-  }
-
-  /**
-  * Store a newly created resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @return \Illuminate\Http\Response
-  */
-  public function store(CreateKeyRequest $request)
-  {
-
-
-
-    $user = User::find(Auth::user()->id);
-    $validated = $request->validated();
-
-    $key = new Key;
-    $key->name = $request->input('keyName');
-    $key->device = 2;
-    $key->user_id = $user->id;
-    $key->lock_id = $request->input('lock');
-    $notification = new Notification;
-    $notification->title = "Se ha creado la llave ".$key->name;
-    $notification->message = "Has creado la llave ".$key->name." para la cerradura ".$key->lock->name." el ".date("Y-m-d H:i:s");
-    $notification->marker = 4;
-    $notification->read = 1;
-    $notification->user_id = Auth::user()->id;
-    $notification->lock_id = $key->lock->id;
-    $notification->key_id = $key->id;
-    $hashed = Hash::make($key->device.$key->user_id.$key->lock_id, [
-      'rounds' => 12
-    ]);
-    //Aqui faltan cosas
-    $key->save();
-    Storage::put("/storage/keys/".time().".key", $hashed);
-    return Storage::download("/storage/keys/".time().".key");
-  }
-
-  /**
-  * Display the specified resource.
-  *
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function show($id)
-  {
-
-  }
-
-  /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function edit($id)
-  {
-    if (Key::where('id',$id)->exists()) {
-      $key= Key::find($id);
-      if (Auth::user()->id == $key->user_id) {
-        return view('pages/key/editKey')->with('key', $key);
-      }else{
-        abort(404);
-      }
-    }else{
-      abort(404);
+    
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
-  }
-
-  /**
-  * Update the specified resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function update(KeyEditRequest $request, $id)
-  {
-    $validated = $request->validated();
-
-    if (Key::where('id',$id)->exists()) {
-      $key=Key::find($id);
-      if (Auth::user()->id == $key->user_id){
-        $notification = new Notification;
-        $notification->title = "Se ha actualizado la llave ".$key->name;
-        $notification->message = "Has actualizado la llave ".$key->name." para la cerradura ".$key->lock->name." el ".date("Y-m-d H:i:s");
-        $notification->marker = 4;
-        $notification->read = 1;
-        $notification->user_id = Auth::user()->id;
-        $notification->lock_id = $key->lock->id;
-        $notification->key_id = $key->id;
-        $notification->save();
-        $key->name = $request->input('newKeyName');
-        $key->save();
-
-        //return view('pages/key/editKey',['key'=>$key]);
-        return redirect()->action('KeyController@edit',['key'=>$key]);
-      }else{
-        abort(404);
-      }
-    }else{
-      abort(404);
+    
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        
+        $keys = Key::where('user_id', Auth::user()->id)->get();
+        
+        return view('pages/key/keys',['keys'=>$keys]);
+        
+        
     }
-
+    
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        return view('pages/key/create');
+    }
+    
     /**
     * Store a newly created resource in storage.
     *
@@ -157,17 +55,25 @@ class KeyController extends Controller
     */
     public function store(CreateKeyRequest $request)
     {
-
-
-
+        
+        
+        
         $user = User::find(Auth::user()->id);
         $validated = $request->validated();
-
+        
         $key = new Key;
         $key->name = $request->input('keyName');
         $key->device = 2;
         $key->user_id = $user->id;
         $key->lock_id = $request->input('lock');
+        $notification = new Notification;
+        $notification->title = "Se ha creado la llave ".$key->name;
+        $notification->message = "Has creado la llave ".$key->name." para la cerradura ".$key->lock->name." el ".date("Y-m-d H:i:s");
+        $notification->marker = 4;
+        $notification->read = 1;
+        $notification->user_id = Auth::user()->id;
+        $notification->lock_id = $key->lock->id;
+        $notification->key_id = $key->id;
         $hashed = Hash::make($key->device.$key->user_id.$key->lock_id, [
             'rounds' => 12
             ]);
@@ -176,7 +82,7 @@ class KeyController extends Controller
             Storage::put("/storage/keys/".time().".key", $hashed);
             return Storage::download("/storage/keys/".time().".key");
         }
-
+        
         /**
         * Display the specified resource.
         *
@@ -185,9 +91,9 @@ class KeyController extends Controller
         */
         public function show($id)
         {
-
+            
         }
-
+        
         /**
         * Show the form for editing the specified resource.
         *
@@ -196,7 +102,6 @@ class KeyController extends Controller
         */
         public function edit($id)
         {
-
             if (Key::where('id',$id)->exists()) {
                 $key= Key::find($id);
                 if (Auth::user()->id == $key->user_id) {
@@ -208,7 +113,7 @@ class KeyController extends Controller
                 abort(404);
             }
         }
-
+        
         /**
         * Update the specified resource in storage.
         *
@@ -219,12 +124,22 @@ class KeyController extends Controller
         public function update(KeyEditRequest $request, $id)
         {
             $validated = $request->validated();
-
+            
             if (Key::where('id',$id)->exists()) {
                 $key=Key::find($id);
                 if (Auth::user()->id == $key->user_id){
+                    $notification = new Notification;
+                    $notification->title = "Se ha actualizado la llave ".$key->name;
+                    $notification->message = "Has actualizado la llave ".$key->name." para la cerradura ".$key->lock->name." el ".date("Y-m-d H:i:s");
+                    $notification->marker = 4;
+                    $notification->read = 1;
+                    $notification->user_id = Auth::user()->id;
+                    $notification->lock_id = $key->lock->id;
+                    $notification->key_id = $key->id;
+                    $notification->save();
                     $key->name = $request->input('newKeyName');
                     $key->save();
+                    
                     //return view('pages/key/editKey',['key'=>$key]);
                     return redirect()->action('KeyController@edit',['key'=>$key]);
                 }else{
@@ -234,7 +149,12 @@ class KeyController extends Controller
                 abort(404);
             }
         }
-
+        
+        
+        
+        
+        
+        
         /**
         * Remove the specified resource from storage.
         *
@@ -244,7 +164,7 @@ class KeyController extends Controller
         public function destroy($id)
         {
             if (Key::where('id',$id)->exists()) {
-
+                
                 $key = Key::find($id);
                 if (Auth::user()->id == $key->user_id){
                     $key->delete();
@@ -258,6 +178,7 @@ class KeyController extends Controller
                 abort(404);
             }
         }
+        
+        
     }
-  }
-}
+    
