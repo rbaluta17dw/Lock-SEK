004a931b91869dd83eb7c7775e3eaa1430839ef6
ibaiway <ibaialberdi@gmail.com>
2019-01-23 21:50:01 +0100

    more cambios

--- app/Http/Controllers/KeyController.php
+++ app/Http/Controllers/KeyController.php
@@ -154,7 +154,7 @@ class KeyController extends Controller
         * @param  int  $id
         * @return \Illuminate\Http\Response
         */
-      
+
 
         /**
         * Show the form for editing the specified resource.
@@ -162,19 +162,7 @@ class KeyController extends Controller
         * @param  int  $id
         * @return \Illuminate\Http\Response
         */
-        public function edit($id)
-        {
-            if (Key::where('id',$id)->exists()) {
-                $key= Key::find($id);
-                if (Auth::user()->id == $key->user_id) {
-                    return view('pages/key/editKey')->with('key', $key);
-                }else{
-                    abort(404);
-                }
-            }else{
-                abort(404);
-            }
-        }
+      
 
         /**
         * Update the specified resource in storage.
