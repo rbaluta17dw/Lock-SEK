e5999dfdd963ba7e9d6b4e5575537da4a417473a
ibaiway <ibaialberdi@gmail.com>
2019-01-28 18:39:50 +0100

    Solucionao el error quisa noloseh no soy cientifico

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -15,7 +15,6 @@
   <div id="wrapper">
 
     <!-- Navigation -->
-    <!-- {!!Auth::user()->roleId == 2 ?  'oscuro' : ""!!}  -->
     <nav class="navbar navbar-default navbar-static-top " role="navigation" style="margin-bottom: 0">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
