662638587bae9d7595d2bce901bd5a7caad91305
rbaluta17dw <rbaluta17dw@zubirimanteo.com>
2019-02-07 10:06:28 +0100

    mas merges

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -146,7 +146,6 @@
         <div class="chartjs-wrapper">
           <iframe class="chartjs-hidden-iframe" style="display: block; overflow: hidden; border: 0px none; margin: 0px; top: 0px; left: 0px; bottom: 0px; right: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;" tabindex="-1">
           </iframe>
-<<<<<<< HEAD
           <canvas id="densityChart" width="600" height="400"></canvas>
           <script>
 
@@ -190,57 +189,7 @@
             data: planetData,
             options: chartOptions
           });
-=======
 
-
-
-          <canvas id="densityChart" width="600" height="400"></canvas>
-
-          <script>
-
-
-            var densityCanvas = document.getElementById("densityChart");
-
-
-
-            var densityData = {
-              label: 'Usuarios basicos',
-              data: [300, 100, 200, 150, 100, 250, 278, 196],
-              backgroundColor: 'rgba(0, 99, 132, 0.6)',
-              borderWidth: 0,
-            };
-
-            var gravityData = {
-              label: 'Usuarios Premium',
-              data: [10, 50, 80, 70, 60, 55, 100, 74],
-              backgroundColor: 'rgba(99, 132, 0, 0.6)',
-              borderWidth: 0,
-
-            };
-
-            var planetData = {
-              labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agoooosto"],
-              datasets: [densityData, gravityData]
-            };
-
-            var chartOptions = {
-              scales: {
-                xAxes: [{
-                  barPercentage: 1,
-                  categoryPercentage: 0.6
-                }],
-                yAxes: [{
-                  id: "y-axis-density"
-                }, ]
-              }
-            };
-
-            var barChart = new Chart(densityCanvas, {
-              type: 'bar',
-              data: planetData,
-              options: chartOptions
-            });
->>>>>>> 0e141a5c261c28077a4213c46bbd0bf47d3dd2f6
 
 
           </script>
@@ -251,7 +200,6 @@
     </div>
     <!-- /.panel -->
   </div>
-<<<<<<< HEAD
   <!-- line chart canvas element -->
 
   <!-- bar chart canvas element -->
@@ -440,97 +388,3 @@
 
 
 @stop
-=======
-  <!-- /.panel -->
-</div>
-<!-- line chart canvas element -->
-
-        <!-- bar chart canvas element -->
-
-        <div class="col-lg-6">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              Login/Registros
-            </div>
-            <!-- /.panel-heading -->
-            <div class="panel-body">
-              <div class="chartjs-wrapper">
-                <iframe class="chartjs-hidden-iframe" style="display: block; overflow: hidden; border: 0px none; margin: 0px; top: 0px; left: 0px; bottom: 0px; right: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;" tabindex="-1">
-                </iframe>
-
-
-
-                <canvas id="Chartnuevo" width="600" height="400"></canvas>
-
-                <script>
-
-
-                  new Chart(document.getElementById("Chartnuevo").getContext("2d"), getChartJs());
-function getChartJs(type) {
-                config = {
-                    type: 'line',
-                    data: {
-                        labels: [
-          @for($i = count($months) -1; $i >= 0; $i--)
-              @if($i!=0)
-              "@lang('adminDashboard.month'.$months[$i])",
-              @else
-              "@lang('adminDashboard.month'.$months[$i])"
-              @endif
-            @endfor
-                              ],
-                        datasets: [{
-                            label: "Registros",
-                            data: [
-
-                           @for ($i = count($registros)-1; $i >= 0; $i--)
-                                 @if($i!=0)
-                                  {{$registros[$i]}},
-                                 @else
-                                        {{$registros[$i]}}
-                                 @endif
-                             @endfor
-                              ],
-                            borderColor: 'rgba(0, 188, 212, 0.75)',
-                            backgroundColor: 'rgba(0, 188, 212, 0.3)',
-                            pointBorderColor: 'rgba(0, 188, 212, 0)',
-                            pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
-                            pointBorderWidth: 1
-                        }, {
-                                label: "My Second dataset",
-                                data: [28, 48, 40, 19, 86, 27, 90, 12, 52, 32, 42, 23],
-                                borderColor: 'rgba(233, 30, 99, 0.75)',
-                                backgroundColor: 'rgba(233, 30, 99, 0.3)',
-                                pointBorderColor: 'rgba(233, 30, 99, 0)',
-                                pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
-                                pointBorderWidth: 1
-                            }]
-                    },
-                    options: {
-                        responsive: true,
-                        legend: false
-                    }
-                }
-              }
-              return config;
-            }
-
-            //          var barChart = new Chart(densityCanvas, {
-              //            type: 'bar',
-              //            data: planetData,
-              //            options: chartOptions
-              //          });
-
-
-            </script>
-
-          </div>
-        </div>
-        <!-- /.panel-body -->
-      </div>
-      <!-- /.panel -->
-    </div>
-
-
-    @stop
->>>>>>> 0e141a5c261c28077a4213c46bbd0bf47d3dd2f6
