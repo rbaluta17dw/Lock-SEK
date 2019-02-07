ab05dea03e96dba014b4daaf7fe21f83847f4c29
Ibai <ibaialberdi@gmail.com>
2019-02-07 10:04:18 +0100

    vista verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -6,6 +6,23 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+          <div class="card">
+                                  <div class="header bg-cyan">
+                                      <h2>
+                                          {{ __('Verifique su correo electronico') }}
+                                      </h2>
+
+                                  </div>
+                                  <div class="body">
+                                    @if (session('resent'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ __('Un link de verificacion se ha enviado a su email.') }}
+                                        </div>
+                                    @endif
+                                    {{ __('Antes de proceder por favor confirme el email que le hemos enviado.') }}
+                                    {{ __('Si no recibio el email ') }}, <a href="{{ route('verification.resend') }}">{{ __('haga click aqui para reenviar') }}</a>.
+                                  </div>
+                              </div>
             <div class="card">
                 <div class="card-header">{{ __('Verifique su correo electronico') }}</div>
 
