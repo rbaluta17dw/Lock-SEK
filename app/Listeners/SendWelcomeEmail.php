d8b14d55e807f1aef74c069f69ccc5658dbeaa8d
ibaiway <ibaialberdi@gmail.com>
2019-02-13 05:04:33 +0100

    Envio email de bienvenida despues de verificar email

---
+++ app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Mail\Welcome;
+use Auth;
+use Mail;
+
+class SendWelcomeEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+     public function handle(Verified $event)
+     {
+
+         Mail::to($event->user->email)->send(new Welcome());
+
+     }
+}
---
+++ app/Mail/Welcome.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Auth;
+
+class Welcome extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+      $user = Auth::user();
+        return $this->view('emails.welcome')->with(['user',$user,]);
+    }
+}
